02cbed4f0b02a914c8a43498135d8a234a8f114e
seungsuuu <tmdtn013@naver.com>
2026-08-29 20:01:33 +0900

    컴포넌트 스캔으로 스프링 빈 등록

--- src/main/java/com/practice/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/com/practice/hellospring/repository/MemoryMemberRepository.java
@@ -1,9 +1,11 @@
 package com.practice.hellospring.repository;
 
 import com.practice.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class MemoryMemberRepository implements MemberRepository{
 
     private static Map<Long, Member> store = new HashMap<>();
